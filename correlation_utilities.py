0c4ecd1429ed8144772a5466889b0a54572ef7b8
Harvey Nguyen <me@ngvuong.com>
2020-05-22 10:42:15 +0700

    Function to display coef and pvalue

--- correlation_utilities.py
+++ correlation_utilities.py
@@ -4,6 +4,7 @@ def get_corr_visualization(df, independent_name, dependent_name):
 	independent variable and dependent variable.
 
 	Requirement: 
+		import matplotlib as mpl
 		import matplotlib.pyplot as plt
 		import seaborn as sns
 		%matplotlib inline
@@ -31,6 +32,31 @@ def get_corr_visualization(df, independent_name, dependent_name):
 	plt.close()
 
 def get_corr_stats(df, independent_name, dependent_name):
+	'''
+	Returns the coffeficient and p-value determine the relationship between
+	independent variable and dependent variable.
+	
+	Description:
+		The resulting coefficient is a value between -1 and 1 inclusive, where:
+			1: Positive linear correlation.
+			0: No linear correlation.
+			-1: Negative linear correlation.
+		P-value by convention, when: 
+			p-value <  0.001: strong evidence that the correlation is significant.
+			p-value <  0.05: moderate evidence that the correlation is significant.
+			p-value <  0.1: weak evidence that the correlation is significant.
+			p-value >  0.1: no evidence that the correlation is significant.
+
+	Requirement: 
+		from scipy import stats
+
+	Parameters:
+		df (DataFrame): The pandas DataFrame.
+		independent_name (str): An independent variable's name.
+		dependent_name (str): A dependent variable's name.
+
+	'''
+
 	x = df[independent_name]
 	y = df[dependent_name]
 	coef, pvalue = stats.pearsonr(x, y)
