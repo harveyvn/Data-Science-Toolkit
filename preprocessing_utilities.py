3947a39ed786ba95d89eddc092462d29b943c2b0
Harvey Nguyen <me@ngvuong.com>
2020-05-28 17:44:37 +0700

    Avoid column name being int

--- preprocessing_utilities.py
+++ preprocessing_utilities.py
@@ -1,43 +1,43 @@
 def count_missing_value(df):
-	'''
-	Counting missing values for each column.
+  '''
+  Counting missing values for each column.
 
-	Parameters:
-		df (DataFrame): The pandas DataFrame.
+  Parameters:
+    df (DataFrame): The pandas DataFrame.
 
-	'''
+  '''
 
-	for column in df.columns:
-		print("number of NaN values for the column " + column + 
-					" :", df[column].isnull().sum())
+  for column in df.columns:
+    print("number of NaN values for the column " + str(column) + 
+          " :", df[column].isnull().sum())
 
 def replace_missing_value_with_mean(df, column_name):
-	'''
-	Replacing the missing values of the column 'column_name' with the 
-	mean of the column 'column_name'  using the method replace()
+  '''
+  Replacing the missing values of the column 'column_name' with the 
+  mean of the column 'column_name'  using the method replace()
 
-	Requirement: 
-		import numpy as np
+  Requirement: 
+    import numpy as np
 
-	Parameters:
-		df (DataFrame): The pandas DataFrame.
-		column_name (str): A column's name.
+  Parameters:
+    df (DataFrame): The pandas DataFrame.
+    column_name (str): A column's name.
 
-	'''
+  '''
 
-	mean=df[column_name].mean()
-	df[column_name].replace(np.nan,mean, inplace=True)
+  mean=df[column_name].mean()
+  df[column_name].replace(np.nan,mean, inplace=True)
 
 
 def count_rows_category(df, cate_column_name):
-	'''
-	Count the number of rows with unique column values and return a dataframe
+  '''
+  Count the number of rows with unique column values and return a dataframe
 
-	Parameters:
-		df (DataFrame): The pandas DataFrame.
-		cate_column_name (str): A categorical column's name.
-	'''
+  Parameters:
+    df (DataFrame): The pandas DataFrame.
+    cate_column_name (str): A categorical column's name.
+  '''
 
-	df_val_count = df[cate_column_name].value_counts().to_frame().reset_index()
-	df_val_count.columns = [cate_column_name, 'count']
-	return df_val_count
+  df_val_count = df[cate_column_name].value_counts().to_frame().reset_index()
+  df_val_count.columns = [cate_column_name, 'count']
+  return df_val_count
